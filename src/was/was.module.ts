bfc6292f238b1faa4492172121b616c2f72b8d77
JasonCatze <epzmfhs@gmail.com>
2024-07-17 17:53:53 +0900

    :recycle: refactor : Change directory change related codes

--- src/was/was.module.ts
+++ src/was/was.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { WasService } from './was.service';
-import { EmailLoginController } from './controllers/email.login.controller';
-import { EmailSignupController } from './controllers/email.signup.controller';
+import { EmailLoginController } from './email.login.controllers/email.login.controller';
+import { EmailSignupController } from './email.login.controllers/email.signup.controller';
 import { DatabaseModule } from 'src/providers/databse/database.module';
 import { RepositoryModule } from 'src/repository/repository.module';
 import { APP_FILTER } from '@nestjs/core';
